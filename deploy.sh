f6ecb54a776070a5193836140d824e21b5b9df09
初志鑫 <1204505056@qq.com>
2020-04-12 02:32:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ git push -f "https://${access_token}@github.com/chuzhixin/vue-admin-beautiful.gi
 git push -f "https://${access_token}@gitee.com/chu1204505056/vue-admin-beautiful.git" master
 cd -
 cd -
+rimraf dist
 exec /bin/bash
