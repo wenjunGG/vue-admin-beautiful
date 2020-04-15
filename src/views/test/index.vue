d8862dce6b551011dd122c2287a9ce82a71a8aa6
初志鑫 <1204505056@qq.com>
2020-04-15 14:19:32 +0800

    deploy

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -13,7 +13,12 @@
       <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
         <el-card
           shadow="never"
-          style="height: 335px; line-height: 30px; font-weight: bold;"
+          style="
+            height: 335px;
+            line-height: 30px;
+            font-weight: bold;
+            overflow-y: scroll;
+          "
         >
           在这里你能学到更多的知识,包括eslint自动的修复而不是手动--fix(千万不要去掉规范,规范是每个人必备的技能),
           包括组件的快速搭建与入手,你的所有问题都会第一时间得到解答,你同时还可以获得一手的学习资料,不要去相信网上好几年前的教程,对技术没有任何的提升,
